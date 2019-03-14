52f8920077a8c432be723eb9b152bd2d2f151387
Bailey Carlson <bcarlson9067@jh-mba158-02.local>
2019-03-14 11:15:30 -0500

    finished app!

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import SafariServices
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
@@ -15,6 +16,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     let locationManager = CLLocationManager()
     var currentLocation: CLLocation!
     var parks: [MKMapItem] = []
+    
+    var initialRegion: MKCoordinateRegion!
+    var isInitialMapLoad = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +32,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         mapView.delegate = self
         
     }
-    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if isInitialMapLoad {
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialMapLoad = false
+        }
+    }
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation.isEqual(mapView.userLocation) {
             return nil
@@ -38,12 +47,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             pin.image = UIImage(named: "ParkPinIcon")
             pin.canShowCallout = true
             let button = UIButton(type: .detailDisclosure)
+            let secondButton = UIButton(type: .contactAdd)
             pin.rightCalloutAccessoryView = button
+            pin.leftCalloutAccessoryView = secondButton
             return pin
         }
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let buttonPressed = control as! UIButton
+        if buttonPressed.buttonType == .contactAdd {
+            mapView.setRegion(initialRegion, animated: true)
+        }
         var currentMapItem = MKMapItem()
         if let title = view.annotation?.title, let parkName = title {
             for mapItem in parks {
@@ -52,8 +67,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                 }
             }
         }
-        let placemark = currentMapItem.placemark
-        print(placemark)
+        
+        if let url = currentMapItem.url {
+            let safariVC = SFSafariViewController(url: url)
+            present(safariVC, animated: true, completion: nil)
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -70,8 +88,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     @IBAction func whenSearchButtonPressed(_ sender: Any) {
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = "Parks"
-        let span = MKCoordinateSpan.init(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.naturalLanguageQuery = "Park"
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.1, longitudeDelta: 0.1)
         request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
         let search = MKLocalSearch(request: request)
         search.start { (response, error) in
@@ -81,10 +99,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             for mapItem in response.mapItems {
                 self.parks.append(mapItem)
                 let annotation = MKPointAnnotation()
-                
                 annotation.coordinate = mapItem.placemark.coordinate
                 annotation.title = mapItem.name
                 self.mapView.addAnnotation(annotation)
+                let placemark = mapItem.placemark
+                if let address = placemark.addressDictionary {
+                    if let street = address["Street"] {
+                        annotation.subtitle = "\(street)"
+                    }
+                }
             }
         }
     }
